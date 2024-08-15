71dc72f4fc0876de0c4f7556f200cd1614dbdfc7
Ferlucena <ferlucena@hotmail.com>
2024-08-15 18:54:19 -0300

    Arreglos de funcionamiento en JWT
    Se agregó una configuracion de seguridad en la clase SecurityConfig

---
+++ src/main/java/com/gc/news_subscription_bot/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.gc.news_subscription_bot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login", "/subscriptions", "/subscriptions/*").permitAll()  // Permitir sin autenticación
+                        .anyRequest().authenticated()  // Cualquier otra solicitud requiere autenticación
+                )
+                .csrf(csrf -> csrf.disable());   // Deshabilitar CSRF para APIs, si aplicable
+
+        return http.build();
+    }
+}
