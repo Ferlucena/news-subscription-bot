ba315920fd0e113f0f1a81f6fa092a6ced184b39
Ferlucena <ferlucena@hotmail.com>
2024-08-15 15:06:33 -0300

    Actualizacion de CorsFilter

--- src/main/java/com/gc/news_subscription_bot/filter/CorsFilter.java
+++ src/main/java/com/gc/news_subscription_bot/filter/CorsFilter.java
@@ -11,22 +11,35 @@ import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 
 /*
 * CorsFilter permite la conexión del front desde puertos distintos
 * */
 
-@WebFilter(urlPatterns = { "/*" })
+@Component
 public class CorsFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
 
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+
         // Añadimos los encabezados CORS a la respuesta para permitir cualquier origen
         ((HttpServletResponse) response).addHeader("Access-Control-Allow-Origin", "*");
         ((HttpServletResponse) response).addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         ((HttpServletResponse) response).addHeader("Access-Control-Allow-Headers", "*");
+
+        // Opcional: Permitir credenciales en las solicitudes CORS
+        // httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+
+        // Si es una solicitud OPTIONS, responde con el código de estado 200
+        if ("OPTIONS".equalsIgnoreCase(httpRequest.getMethod())) {
+            httpResponse.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
 
         //Continuamos con el siguiente filtro de la cadena
         chain.doFilter(request, response);
