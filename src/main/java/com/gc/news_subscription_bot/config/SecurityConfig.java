3ab7a123549a8300fc049f1a8efd5f8ebaa97d74
Ferlucena <ferlucena@hotmail.com>
2024-08-15 20:58:39 -0300

    Arreglo de la configuracion CORS API-FETCH
    Se colocaron configuraciones web en general CORS, y se etablecio la conexión api-fetch

--- src/main/java/com/gc/news_subscription_bot/config/SecurityConfig.java
+++ src/main/java/com/gc/news_subscription_bot/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    //Filtro para token
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
