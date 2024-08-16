22aa187f4dd5f6c5f73cbb85a1dd6a999585e8cf
Ferlucena <ferlucena@hotmail.com>
2024-08-16 12:51:44 -0300

    Update login-fetch.js
    Correccion del token reccibido

--- src/frontend/src/services/login-fetch.js
+++ src/frontend/src/services/login-fetch.js
@@ -10,10 +10,16 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         },
         body: JSON.stringify({ username, password })
     })
-        .then(response => response.json())
+        .then(response => {
+            if (response.ok && response.headers.get('Content-Type').includes('application/json')) {
+                return response.json();
+            } else {
+                throw new Error('No JSON response');
+            }
+        })
         .then(data => {
             if(data.token) {
-                localStorage.setItem('token', data.token);
+                localStorage.setItem('token', data.tw);
                 alert('Inicio de sesión exitoso');
                 // Redireccionar al usuario a otra página o actualizar la interfaz de usuario
             } else {
